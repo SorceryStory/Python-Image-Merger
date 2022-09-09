48aed15ce3d1f7b1558bc56603e039e729dfe14a
Joana Almeida <sorceress.spell@gmail.com>
2022-09-09 05:48:49 +0100

    Added the script and some test images.

---
+++ ImageMerger.py
@@ -0,0 +1,133 @@
+import csv
+import getopt
+import os
+import sys
+
+from PIL import Image, ImageColor
+
+
+def merge_images(image1_path: str, image2_path: str, horizontal: bool, mode: str, fill_color: tuple):
+    try:
+        image1 = Image.open(image1_path)
+        image2 = Image.open(image2_path)
+
+        if horizontal:
+            finalWidth = image1.width + image2.width
+            finalHeight = max(image1.height, image2.height)
+        else:
+            finalWidth = max(image1.width, image2.width)
+            finalHeight = image1.height + image2.height
+
+        mergedImage = Image.new(mode=mode, size=(
+            finalWidth, finalHeight), color=fill_color)
+
+        if horizontal:
+            mergedImage.paste(image1, (0, 0))
+            mergedImage.paste(image2, (image1.width, 0))
+        else:
+            mergedImage.paste(image1, (0, 0))
+            mergedImage.paste(image2, (0, image1.height))
+
+        return mergedImage, True
+    except:
+        return None, False
+
+
+def merge_and_save(image1_path: str, image2_path: str, horizontal: bool, mode: str, fill_color: tuple, final_path: str, optimize: bool):
+    try:
+        finalImage, success = merge_images(image1_path=image1_path, image2_path=image2_path,
+                                           horizontal=horizontal, mode=mode, fill_color=fill_color)
+        if success:
+            dirname = os.path.dirname(final_path)
+            if len(dirname) > 0:
+                os.makedirs(dirname, exist_ok=True)
+            finalImage.save(fp=final_path, optimize=optimize)
+            return finalImage, True
+        else:
+            return None, False
+    except:
+        return None, False
+
+
+def print_merge_message(image1_path: str, image2_path: str, final_path: str, success: bool):
+    print("Merge", image1_path, "with", image2_path, "into",
+          final_path, "SUCCESS" if success else "FAILURE")
+
+
+def main(argv):
+    image_list_path = ""
+
+    image1_path = ""
+    image2_path = ""
+    final_path = ""
+
+    horizontal = True
+    mode = "RGBA"
+    fill_color = (255, 255, 255, 255)
+    optimize = False
+
+    verbose = False
+
+    option_values, _ = getopt.getopt(args=argv, shortopts="", longopts=[
+                                     "image-list-path=", "image1-path=", "image2-path=", "final-path=", "vertical", "mode=", "fill-color=", "optimize", "verbose"])
+
+    for option_name, option_value in option_values:
+        if option_name == "--image-list-path":
+            image_list_path = option_value
+        elif option_name == "--image1-path":
+            image1_path = option_value
+        elif option_name == "--image2-path":
+            image2_path = option_value
+        elif option_name == "--final-path":
+            final_path = option_value
+        elif option_name == "--vertical":
+            horizontal = False
+        elif option_name == "--mode":
+            mode = option_value
+        elif option_name == "--fill-color":
+            print("COLOR", option_value)
+            fill_color = ImageColor.getrgb(option_value)
+        elif option_name == "--optimize":
+            optimize = True
+        elif option_name == "--verbose":
+            verbose = True
+
+    if verbose:
+        print("--- Merge Arguments ---")
+        if len(image_list_path) > 0:
+            print("image_list_path:", image_list_path)
+        else:
+            print("    image1_path:", image1_path)
+            print("    image2_path:", image2_path)
+            print("     final_path:", final_path)
+        print("     horizontal:", horizontal)
+        print("           mode:", mode)
+        print("     fill_color:", fill_color)
+        print("       optimize:", optimize)
+        print("-----------------------")
+
+    if len(image_list_path) > 0:
+        try:
+            with open(image_list_path, newline='') as csv_file:
+                csv_reader = csv.reader(
+                    csv_file, delimiter=',', escapechar="\\")
+                for row in csv_reader:
+                    _, success = merge_and_save(image1_path=row[0], image2_path=row[1], horizontal=horizontal,
+                                                mode=mode, fill_color=fill_color, final_path=row[2], optimize=optimize)
+
+                    print_merge_message(
+                        image1_path=row[0], image2_path=row[1], final_path=row[2], success=success)
+
+        except FileNotFoundError:
+            print("Could not process", image_list_path, ": File Not Found")
+
+    else:
+        _, success = merge_and_save(image1_path=image1_path, image2_path=image2_path, horizontal=horizontal,
+                                    mode=mode, fill_color=fill_color, final_path=final_path, optimize=optimize)
+
+        print_merge_message(image1_path=image1_path, image2_path=image2_path,
+                            final_path=final_path, success=success)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# ImageMerger
-An Image Merging Script
+# Python Image Merger
+
+A Python image merging script.
+
+## Dependencies
+
+This scrip uses [Pillow](https://pillow.readthedocs.io/en/stable/index.html#) for image manipulation. Please [install Pillow](https://pillow.readthedocs.io/en/stable/installation.html) before using this script.
+
+## Usage
+
+There are two main ways to use this script. You can either merge 2 images or use a .csv file to batch image merging.
+
+### Merging 2 Images
+
+```console
+python ImageMerger.py --image1-path 'test_files/orange.png' --image2-path 'test_files/scarlet_red.png' --final-path 'test_files/output/test_vertical.png' --vertical --mode 'RGB' --fill-color 'rgb(255,255,255)' --optimize --verbose
+```
+
+- `--image1-path [str]`: The path to the first image, relative to the current running directory.
+- `--image2-path [str]`: The path to the second image, relative to the current running directory.
+- `--final-path [str]`: The path where to store the merged image, relative to the current running directory.
+- `--vertical`: Include if you want the merge to happen vertically. Optional. Default is horizontal.
+- `--mode [str]`: A string defining the type and depth of the pixels in the final image. Refer to [Pillow modes](https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes) for all possible options. Optional. Default is `RGBA`.
+- `fill-color [str]`: A string defining the final image's background color (only visible when images have mismatched sizes). Refer to [Pillow color names](https://pillow.readthedocs.io/en/stable/reference/ImageColor.html#color-names) for all possible options. Optional. Default is `#ffffffff` or `(255, 255, 255, 255)`.
+- `--optimize`: Include if you want the final image to be optimized. Optional.
+- `--verbose`: Include if you want the program to output to the console the value of all setup variables. Optional.
+
+### Using a CSV file
+
+```console
+python ImageMerger.py --image-list-path 'test_files/test_image_list.csv' --vertical --mode 'RGB' --fill-color 'rgb(255,255,255)' --optimize --verbose
+```
+
+- `--image-list-path [str]`: The path to the [.csv file](#csv-file-structure), relative to the current running directory.
+- `--vertical`: Include if you want the merges to happen vertically. Optional. Default is horizontal.
+- `--mode [str]`: A string defining the type and depth of the pixels in the final images. Refer to [Pillow modes](https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes) for all possible options. Optional. Default is `RGBA`.
+- `fill-color [str]`: A string defining the final images' background color (only visible when images have mismatched sizes). Refer to [Pillow color names](https://pillow.readthedocs.io/en/stable/reference/ImageColor.html#color-names) for all possible options. Optional. Default is `#ffffffff` or `(255, 255, 255, 255)`.
+- `--optimize`: Include if you want the final images to be optimized. Optional.
+- `--verbose`: Include if you want the program to output to the console the value of all setup variables. Optional.
+
+#### CSV File Structure
+
+```csv
+image1_path,image2_path,final_path1
+image3_path,image4_path,final_path2
+image5_path,image6_path,final_path3
+[...]
+```
+
+Note that these paths should not have any quotes around them. Here's an example:
+
+```csv
+test_files/chameleon.png,test_files/chameleon.png,test_files/output/test_csv1.png
+test_files/sky_blue.png,test_files/plum.png,test_files/output/test_csv2.png
+test_files/aluminium_light.png,test_files/aluminium_dark.png,test_files/output/test_csv3.png
+```
